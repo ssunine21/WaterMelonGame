7a4958f8461f9c50201396c9f93516a3d0173690
ssunine21 <ssunine21@gmail.com>
2023-09-21 07:29:01 +0900

    [#fix]
    아이콘 옮기기

--- Assets/Scripts/ViewCanvas/ViewCanvasOption.cs
+++ Assets/Scripts/ViewCanvas/ViewCanvasOption.cs
@@ -7,14 +7,14 @@ using TMPro;
 
 public class ViewCanvasOption : ViewCanvas {
     public ButtonExpansion[] Close => _buttonClose;
-    public UltimateClean.Switch Music => _switchMusic;
-    public UltimateClean.Switch Effect => _switchEffect;
-    public UltimateClean.Switch Vibration => _switchVibration;
+    public Switch Music => _switchMusic;
+    public Switch Effect => _switchEffect;
+    public Switch Vibration => _switchVibration;
 
     [SerializeField] private ButtonExpansion[] _buttonClose;
-    [SerializeField] private UltimateClean.Switch _switchMusic;
-    [SerializeField] private UltimateClean.Switch _switchEffect;
-    [SerializeField] private UltimateClean.Switch _switchVibration;
+    [SerializeField] private Switch _switchMusic;
+    [SerializeField] private Switch _switchEffect;
+    [SerializeField] private Switch _switchVibration;
 
     [SerializeField] private TMP_Text _textBGM;
     [SerializeField] private TMP_Text _textEffect;
