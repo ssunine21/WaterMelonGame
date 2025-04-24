78aa62a3881ae21a7f0f8742ca0886c19bccd754
ssunine21 <ssunine21@gmail.com>
2025-04-25 00:24:07 +0900

    clen project

--- Assets/Scripts/Player/PlayerBook.cs
+++
@@ -1,8 +0,0 @@
-    using UnityEngine.Events;
-
-    public class PlayerBook
-    {
-        public static UnityAction<int> OnFindBookObject;
-        public static UnityAction<bool> OnBindGetBookReward;
-
-    }
